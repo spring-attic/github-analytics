18816d11e8fd152f882cd35c4104a84b6d033fc0
Dave Syer <dsyer@pivotal.io>
2016-04-14 08:03:18 -0400

    Remove @EnableBinding

--- src/main/java/org/springframework/github/AnalyticsApplication.java
+++ src/main/java/org/springframework/github/AnalyticsApplication.java
@@ -2,13 +2,10 @@ package org.springframework.github;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.app.fieldvaluecounter.sink.FieldValueCounterSinkStoreConfiguration;
-import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@EnableBinding(Sink.class)
 @Import(FieldValueCounterSinkStoreConfiguration.class)
 public class AnalyticsApplication {
 
--- src/test/java/org/springframework/github/AnalyticsApplicationTests.java
+++ src/test/java/org/springframework/github/AnalyticsApplicationTests.java
@@ -1,19 +1,18 @@
 package org.springframework.github;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.cloud.stream.messaging.Sink;
-import org.springframework.cloud.stream.test.binder.MessageCollector;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHeaders;
-import org.springframework.messaging.support.GenericMessage;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.util.MimeTypeUtils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = AnalyticsApplication.class)
@@ -32,7 +31,7 @@ public class AnalyticsApplicationTests {
 		String json = new ObjectMapper().writeValueAsString(data);
 		Message<String> message =
 				MessageBuilder.withPayload(json).setHeader(MessageHeaders.CONTENT_TYPE, MimeTypeUtils.APPLICATION_JSON).build();
-		sink.input().send(message);
+		this.sink.input().send(message);
 
 	}
 
