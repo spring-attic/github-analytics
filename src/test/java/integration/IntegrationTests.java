da0b0c5aaf3d9bc5e3fd5b694d3b33400849330b
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2016-09-04 01:09:09 +0200

    Fixed tests

--- src/test/java/integration/IntegrationTests.java
+++ src/test/java/integration/IntegrationTests.java
@@ -1,13 +1,17 @@
 package integration;
 
+import java.lang.invoke.MethodHandles;
 import java.util.Map;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.BDDAssertions.then;
@@ -22,6 +26,8 @@ import static org.awaitility.Awaitility.await;
 @EnableAutoConfiguration
 public class IntegrationTests {
 
+	private static final Log log = LogFactory.getLog(MethodHandles.lookup().lookupClass());
+
 	@Value("${stubrunner.url}") String stubRunnerUrl;
 	@Value("${application.url}") String applicationUrl;
 
@@ -30,14 +36,16 @@ public class IntegrationTests {
 	@Test
 	public void shouldStoreAMessageWhenGithubDataWasReceived() {
 		final Integer countOfEntries = countGithubData();
-		Map response = triggerMessage();
-		then(response).isNotEmpty();
+		log.info("Initial count is [" + countOfEntries + "]");
+		ResponseEntity<Map> response = triggerMessage();
+		then(response.getStatusCode().is2xxSuccessful()).isTrue();
 
+		log.info("Awaiting proper count of github data");
 		await().until(() -> countGithubData() > countOfEntries);
 	}
 
-	private Map triggerMessage() {
-		return this.testRestTemplate.postForObject("http://" +
+	private ResponseEntity<Map> triggerMessage() {
+		return this.testRestTemplate.postForEntity("http://" +
 				this.stubRunnerUrl + "/triggers/hook_created_v2", "", Map.class);
 	}
 
