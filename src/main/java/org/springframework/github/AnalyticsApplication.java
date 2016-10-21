edeb73e15bd14af0e7ab5ffe6eca05bafc9ff76d
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2016-10-21 11:15:08 +0200

    Fixed missing Eureka impl

--- src/main/java/org/springframework/github/AnalyticsApplication.java
+++ src/main/java/org/springframework/github/AnalyticsApplication.java
@@ -6,6 +6,7 @@ import org.springframework.analytics.metrics.FieldValueCounterRepository;
 import org.springframework.analytics.metrics.memory.InMemoryFieldValueCounterRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Sink;
@@ -15,6 +16,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableBinding(Sink.class)
 public class AnalyticsApplication {
 
