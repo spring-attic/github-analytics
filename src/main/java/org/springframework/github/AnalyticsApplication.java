c7ffa6b2da5d59512f5c88f75a9a90e79029152f
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2018-05-08 01:09:52 +0200

    Added connector manually

--- src/main/java/org/springframework/github/AnalyticsApplication.java
+++ src/main/java/org/springframework/github/AnalyticsApplication.java
@@ -2,8 +2,11 @@ package org.springframework.github;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.java.ServiceScan;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 @EnableBinding(Sink.class)
@@ -11,5 +14,11 @@ public class AnalyticsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AnalyticsApplication.class, args);
+	}
+
+	@Configuration
+	@ServiceScan
+	@Profile("cloud")
+	public class ServiceConfiguration {
 	}
 }
